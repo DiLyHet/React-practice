a5e5b0a20b373b6d50a668f49c64a20bbcf011d1
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2023-10-06 15:10:55 +0300

    task1

---
+++ lesson_1/task_1/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+
+const rootElem = document.querySelector('#root');
+
+const element = React.createElement(
+    'div',
+    { className: 'greeting' },
+    'Hello, React!'
+);
+
+ReactDOM.render(element, rootElem);
