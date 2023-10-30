8e016c35cd68d7f4a4c5ba2d254d622e63dcb7ce
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2023-10-30 14:36:58 +0200

    1

--- tools/task_12/webpack.config.js
+++ tools/task_12/webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -37,6 +38,7 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({
             filename: '[name].css'
-        })
+        }),
+        new CleanWebpackPlugin(),
     ]
 };
