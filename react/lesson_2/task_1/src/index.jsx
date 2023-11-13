7a594aadf02e8bd726ee3fb1caf3eafa8b8e1b64
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2023-11-13 15:14:02 +0200

    1

--- react/lesson_2/task_1/src/index.jsx
+++ react/lesson_2/task_1/src/index.jsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Comment from './Comment.jsx';
+import Greeting from './Greeting.jsx';
 import './index.scss';
-import './comment.scss';
 
 const rootElement = document.querySelector('#root');
 
-export const author = {
-  name: 'Tom',
-  avatarUrl: 'https://images.unsplash.com/photo-1493612276216-ee3925520721?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-};
-console.log(author);
-ReactDOM.render(<Comment author={author} text="Good job!" date={new Date()} />, rootElement);
+ReactDOM.render(<Greeting firstName="John" lastName="Doe" birthDate={new Date("2006-11-11")} />, rootElement);
