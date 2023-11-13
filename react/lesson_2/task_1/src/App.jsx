0ed74c243b92d5a6d34928f8e5b815e7c3ab9b5b
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2023-11-13 18:55:50 +0200

    Update App.jsx

--- react/lesson_2/task_1/src/App.jsx
+++ react/lesson_2/task_1/src/App.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import './index.scss';
 import Clock from './Clock.jsx';
 
-function App() {
-  return (
-    <>
-      <Clock location="New York" offset={-5}/>
-      <Clock location="Kyiv" offset={3}/>
-      <Clock location="London" offset={0}/>
-    </>
-  );
-}
+const App = () => (
+  <>
+    <Clock location="New York" offset={-4} />
+    <Clock location="Kyiv" offset={3} />
+    <Clock location="London" offset={1} />
+  </>
+);
 
 export default App;
