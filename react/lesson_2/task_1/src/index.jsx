2dc2feceafc57f924882837d0842d7db872f14c3
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2023-11-16 12:50:11 +0200

    f

--- react/lesson_2/task_1/src/index.jsx
+++ react/lesson_2/task_1/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import GoodButton from './GoodButton.jsx';
+import Counter from './Counter.jsx';
 
 const root = document.querySelector('#root');
   
-ReactDOM.render(<GoodButton />, root);
+ReactDOM.render(<Counter />, root);
