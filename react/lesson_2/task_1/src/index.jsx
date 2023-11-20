1183118b68b9a4e7b31e561ee2ccee2e15652156
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2023-11-20 17:19:20 +0200

    Update index.jsx

--- react/lesson_2/task_1/src/index.jsx
+++ react/lesson_2/task_1/src/index.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import Mailbox from './Mailbox.jsx';
 
-const messagesArray = ['1', '2', '3', '4'];
-
 const root = document.querySelector('#root');
 
-ReactDOM.render(<Mailbox unreadMessages={['1', '2', '3', '4'].length} />, root);
+ReactDOM.render(<Mailbox unreadMessages={[].length} />, root);
