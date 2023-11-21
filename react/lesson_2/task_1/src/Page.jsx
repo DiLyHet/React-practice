8c6e8e945d417d34a4f852c87f2bfa339e79013c
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2023-11-21 14:51:26 +0200

    Update Page.jsx

--- react/lesson_2/task_1/src/Page.jsx
+++ react/lesson_2/task_1/src/Page.jsx
@@ -31,7 +31,7 @@ class Page extends React.Component {
           <button className="btn" onClick={() => this.setInfo(infoBtn2)}>
             IPhone 13 Pro
           </button>
-          <button className="btn" onClick={() => this.setInfo(undefined)}>
+          <button className="btn" onClick={() => this.setInfo(' ')}>
             Clear
           </button>
         </div>
