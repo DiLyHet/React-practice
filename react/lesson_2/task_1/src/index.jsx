82f38c0af66d62403c5ab9fbf6258eaeb9b03e80
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2023-12-18 14:41:20 +0200

    ss

--- react/lesson_2/task_1/src/index.jsx
+++ react/lesson_2/task_1/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App.jsx';
+import Dimensions from './Dimensions.jsx';
 
 const root = document.querySelector('#root');
 
-ReactDOM.render(<App />, root);
+ReactDOM.render(<Dimensions />, root);
