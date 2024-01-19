476980c08206e2eceda220e33fdc0d445947a2e8
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2024-01-19 14:51:02 +0200

    dfvbg

---
+++ react/lesson_2/task_1/src/Board.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import Square from './Square';
+
+class Board extends React.Component {
+  handleClick = i => {
+    const { squares, onPlay, xIsNext } = this.props;
+    if (this.calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
+    onPlay(nextSquares);
+  };
+
+  renderSquare = i => {
+    const { squares } = this.props;
+    return <Square value={squares[i]} onSquareClick={() => this.handleClick(i)} />;
+  };
+
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  render() {
+    const { xIsNext, squares } = this.props;
+    const winner = this.calculateWinner(squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
+
+    return (
+      <>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </>
+    );
+  }
+}
+
+export default Board;
--- react/lesson_2/task_1/src/index.jsx
+++ react/lesson_2/task_1/src/index.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import UsersList from './UsersList';
+import Game from './Game.jsx';
 
 const root = document.querySelector('#root');
 
-ReactDOM.render(
-  <UsersList
-    users={[
-      { name: 'Lydia', age: 23 },
-      { name: 'Ann', age: 45 },
-    ]}
-  />,
-  root,
-);
+ReactDOM.render(<Game />, root);
