339ea428b71aaf82ab882396a5a1196fa71120e9
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2024-01-23 17:37:54 +0200

    dfgh

--- react/lesson_2/task_1/src/index.jsx
+++ react/lesson_2/task_1/src/index.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Calculator from './Calculator.jsx';
+import FilterableProductTable from './FilterableProductTable';
+
+const PRODUCTS = [
+    {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+    {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+    {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+    {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+    {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+    {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+  ];
 
 const root = document.querySelector('#root');
 
-ReactDOM.render(<Calculator />, root);
+ReactDOM.render(<FilterableProductTable products={PRODUCTS} />, root);
