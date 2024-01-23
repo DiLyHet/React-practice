e3952f2f5b318d10f3205651c5f01e3dfbc6bf74
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2024-01-23 16:01:17 +0200

    vhg

--- react/lesson_2/task_1/src/TemperatureInput.jsx
+++ react/lesson_2/task_1/src/TemperatureInput.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
-
-const scaleNames = {
-  c: 'Celsius',
-  f: 'Fahrenheit',
-};
+import { scaleNames } from './Calculator.jsx';
 
 export default class TemperatureInput extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
-    this.state = { temperature: '' };
   }
 
   handleChange(e) {
-    this.setState({ temperature: e.target.value });
+    this.props.onTemperatureChange(e.target.value);
   }
 
   render() {
-    const temperature = this.state.temperature;
+    const temperature = this.props.temperature;
     const scale = this.props.scale;
     return (
       <fieldset>
