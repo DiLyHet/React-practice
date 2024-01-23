e5c860b6c105b20e1c5aa23b0493f5916173d6ec
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2024-01-23 18:31:55 +0200

    fghj

---
+++ react/lesson_2/task_1/src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import Numbers from './Numbers';
+import EvenNumbers from './EvenNumbers';
+import OddNumbers from './OddNumbers';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      number: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState({
+        number: this.state.number + 1,
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <EvenNumbers title="Even numbers" number={this.state.number} />
+        <Numbers title="All numbers" number={this.state.number} />
+        <OddNumbers title="Odd numbers" number={this.state.number} />
+        <Numbers title="All numbers" number={17} />
+      </div>
+    );
+  }
+}
--- react/lesson_2/task_1/src/index.jsx
+++ react/lesson_2/task_1/src/index.jsx
@@ -1,17 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import FilterableProductTable from './FilterableProductTable';
-
-const PRODUCTS = [
-    {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
-    {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
-    {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
-    {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
-    {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
-    {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
-  ];
+import App from './App';
 
 const root = document.querySelector('#root');
 
-ReactDOM.render(<FilterableProductTable products={PRODUCTS} />, root);
+ReactDOM.render(<App />, root);
