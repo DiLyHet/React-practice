9965907a6375e56718e830704d7964d6dabeb7f2
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2024-01-27 18:35:14 +0200

    ghj

---
+++ react/lesson_2/task_1/src/Expand.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+export default class Expand extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: false,
+    };
+  }
+
+  hideButton = () => {
+    this.setState({
+      isOpen: false,
+    });
+  };
+
+  showButton = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <div className="expand border">
+          <div className="expand__header">
+            <span className="expand__title">{this.props.title}</span>
+            <button className="expand__toggle-btn" onClick={this.showButton}>
+              <i className={this.state.isOpen ? "fas fa-chevron-up" : "fas fa-chevron-down"}></i>
+            </button>
+          </div>
+          <div className="expand__content">
+            <p>{this.state.isOpen ? this.props.content : ''}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- react/lesson_2/task_1/src/index.jsx
+++ react/lesson_2/task_1/src/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
+import Expand from './Expand';
 
 const root = document.querySelector('#root');
 
-ReactDOM.render(<App />, root);
+ReactDOM.render(<Expand title='HOOKS' content='Hooks are a new addition in React 16.8. They let you use state and other React features
+without writing a class.' />, root);
