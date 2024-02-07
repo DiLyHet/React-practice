bf63707772a1871d90287a4f6cd1807d9098e97a
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2024-02-07 23:35:49 +0200

    Update App.jsx

--- react/lesson_2/task_1/src/App.jsx
+++ react/lesson_2/task_1/src/App.jsx
@@ -18,14 +18,14 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div className="app">
+      
         <Expand title={'HOOKS'} showButton={this.showButton} isOpen={this.state.isOpen}>
           <p>
             Hooks are a new addition in React 16.8. They let you use state and other React features
             without writing a class.
           </p>
         </Expand>
-      </div>
+      
     );
   }
 }
