e11a4015261c1800cf4d67f6bdbad61bda815526
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2024-02-08 15:08:03 +0200

    Update Expand.jsx

--- react/lesson_2/task_1/src/Expand.jsx
+++ react/lesson_2/task_1/src/Expand.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 
-const Expand = ({ children, isOpen, title, showButton }) => {
-  return (
-    <div className="expand border">
-      <div className="expand__header">
-        <span className="expand__title">{title}</span>
-        <button className="expand__toggle-btn" onClick={showButton}>
-          <i className={isOpen ? 'fas fa-chevron-up' : 'fas fa-chevron-down'}></i>
-        </button>
+class Expand extends React.Component {
+  state = {
+    isExpanded: false,
+  };
+
+  toggleContent = () => {
+    this.setState({
+      isExpanded: !this.state.isExpanded,
+    });
+  };
+
+  render() {
+    const { isExpanded } = this.state;
+    const { title, children } = this.props;
+    return (
+      <div className="expand border">
+        <div className="expand__header">
+          <span className="expand__title">{title}</span>
+          <button className="expand__toggle-btn" onClick={this.toggleContent}>
+            <i className={isExpanded ? 'fas fa-chevron-up' : 'fas fa-chevron-down'}></i>
+          </button>
+        </div>
+        {isExpanded && <div className="expand__content">{children}</div>}
       </div>
-      {isOpen && <div className="expand__content">{children}</div>}
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Expand;
