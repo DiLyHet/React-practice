ae3b7396439d1b908cc30f4e7154a4d20fbd5909
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2024-02-12 14:35:32 +0200

    vghk

--- react/lesson_2/task_1/src/App.jsx
+++ react/lesson_2/task_1/src/App.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import Home from './Home';
 import Products from './Products';
-import Contacts from './Contacts';
-import PageNotFound from './PageNotFound';
 
 const App = () => {
   return (
-    <div className="page">
+    <div class="page">
       <BrowserRouter>
+        <ul class="navigation">
+          <li class="navigation__item">
+            <Link to="/">Home</Link>
+          </li>
+          <li class="navigation__item">
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
         <Switch>
           <Route exact path="/">
             <Home />
           </Route>
-          <Route path="/Products">
+          <Route path="/products">
             <Products />
-          </Route>
-          <Route path="/Contacts">
-            <Contacts />
-          </Route>
-          <Route path="*">
-            <PageNotFound />
           </Route>
         </Switch>
       </BrowserRouter>
--- react/lesson_2/task_1/src/Products.jsx
+++ react/lesson_2/task_1/src/Products.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Product from './Product';
+import { Link, Route, Switch } from 'react-router-dom';
 
 const Products = () => {
   return (
-    <div className="page__content">
+    <div class="page__content">
       <h1>Products</h1>
-      <p>We will add some products. Later... 😇</p>
-      <Link to="/contacts">Contacts</Link>
+      <ul class="navigation">
+        <li class="navigation__item">
+          <Link to="/products/book">Book</Link>
+        </li>
+        <li class="navigation__item">
+          <Link to="/products/ball">Ball</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path="/products">
+          <span>Select a product, please</span>
+        </Route>
+        <Route path="/products/:productId" component={Product} />
+      </Switch>
     </div>
   );
 };
