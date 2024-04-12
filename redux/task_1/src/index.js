afaafd470a39041c605cbecca1bdbb8c5337c1da
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2024-04-12 17:04:57 +0300

    123

--- redux/task_1/src/index.js
+++ redux/task_1/src/index.js
@@ -3,67 +3,14 @@ import { useDispatch } from 'react-redux';
 import { setLanguage } from './language.actions';
 import { addProduct, removeProduct } from './cart.actions';
 import { setUser, removeUser } from './user.actions';
+import store from './store';
 
-export const LanguageSelector = () => {
-  const dispatch = useDispatch();
-
-  const handleLanguageChange = (language) => {
-    dispatch(setLanguage(language));
-  };
-
-  return (
-    <div>
-      <button onClick={() => handleLanguageChange('en')}>English</button>
-      <button onClick={() => handleLanguageChange('fr')}>French</button>
-      <button onClick={() => handleLanguageChange('it')}>Italian</button>
-      <button onClick={() => handleLanguageChange('ukr')}>Ukrainian</button>
-    </div>
-  );
-};
+store.subscribe(() => {
+  console.log(store.getState());
+});
 
 
-
-
-
-
-export const Cart = () => {
-  const dispatch = useDispatch();
-
-  const handleAddProduct = (product) => {
-    dispatch(addProduct(product));
-  };
-
-  const handleRemoveProduct = (productId) => {
-    dispatch(removeProduct(productId));
-  };
-
-  return (
-    <div>
-      <button onClick={() => handleAddProduct({ id: 1, name: 'milk' })}>Add Milk to Cart</button>
-      <button onClick={() => handleRemoveProduct(1)}>Remove Milk from Cart</button>
-    </div>
-  );
-};
-
-
-
-
-
-export const UserProfile = () => {
-  const dispatch = useDispatch();
-
-  const handleSetUser = (userData) => {
-    dispatch(setUser(userData));
-  };
-
-  const handleRemoveUser = () => {
-    dispatch(removeUser());
-  };
-
-  return (
-    <div>
-      <button onClick={() => handleSetUser({ name: 'Bob' })}>Set User</button>
-      <button onClick={handleRemoveUser}>Remove User</button>
-    </div>
-  );
-};
+store.dispatch(addProduct({id: 1, name: 'milk'}));
+store.dispatch(addProduct({id: 2, name: 'water'}));
+store.dispatch(removeProduct(2));
+store.dispatch(addProduct({id: 3, name: 'banana'}));
